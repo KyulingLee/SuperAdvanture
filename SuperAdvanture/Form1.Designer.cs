d7aa6db00392b8703a761b0adff669640db12021
Kyuling Lee <kyuling@me.com>
2018-06-02 13:27:54 +0900

    초기 커밋. 첫 프로젝트 추가 및 화면 제작중. 엔진 클래스 라이브러리 추가.

--- SuperAdvanture/Form1.Designer.cs
+++ SuperAdvanture/Form1.Designer.cs
@@ -28,12 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.LevelLabel = new System.Windows.Forms.Label();
+            this.ExperienceLabel = new System.Windows.Forms.Label();
+            this.GoldLabel = new System.Windows.Forms.Label();
+            this.HitPointsLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hit Points:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Gold:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Experience:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(18, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 15);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Level:";
+            // 
+            // LevelLabel
+            // 
+            this.LevelLabel.AutoSize = true;
+            this.LevelLabel.Location = new System.Drawing.Point(110, 100);
+            this.LevelLabel.Name = "LevelLabel";
+            this.LevelLabel.Size = new System.Drawing.Size(15, 15);
+            this.LevelLabel.TabIndex = 7;
+            this.LevelLabel.Text = "0";
+            // 
+            // ExperienceLabel
+            // 
+            this.ExperienceLabel.AutoSize = true;
+            this.ExperienceLabel.Location = new System.Drawing.Point(110, 74);
+            this.ExperienceLabel.Name = "ExperienceLabel";
+            this.ExperienceLabel.Size = new System.Drawing.Size(15, 15);
+            this.ExperienceLabel.TabIndex = 6;
+            this.ExperienceLabel.Text = "0";
+            // 
+            // GoldLabel
+            // 
+            this.GoldLabel.AutoSize = true;
+            this.GoldLabel.Location = new System.Drawing.Point(110, 46);
+            this.GoldLabel.Name = "GoldLabel";
+            this.GoldLabel.Size = new System.Drawing.Size(15, 15);
+            this.GoldLabel.TabIndex = 5;
+            this.GoldLabel.Text = "0";
+            // 
+            // HitPointsLabel
+            // 
+            this.HitPointsLabel.AutoSize = true;
+            this.HitPointsLabel.Location = new System.Drawing.Point(110, 20);
+            this.HitPointsLabel.Name = "HitPointsLabel";
+            this.HitPointsLabel.Size = new System.Drawing.Size(15, 15);
+            this.HitPointsLabel.TabIndex = 4;
+            this.HitPointsLabel.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(275, 218);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(717, 643);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LevelLabel);
+            this.Controls.Add(this.ExperienceLabel);
+            this.Controls.Add(this.GoldLabel);
+            this.Controls.Add(this.HitPointsLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Kyuling\'s Game";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label LevelLabel;
+        private System.Windows.Forms.Label ExperienceLabel;
+        private System.Windows.Forms.Label GoldLabel;
+        private System.Windows.Forms.Label HitPointsLabel;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- SuperAdvanture/Form1.cs
+++ SuperAdvanture/Form1.cs
@@ -16,5 +16,10 @@ namespace SuperAdvanture
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GoldLabel.Text = "123";
+        }
     }
 }
