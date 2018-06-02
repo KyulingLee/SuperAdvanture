1ad466ee5ca85b7a9c0847ed9e5e2134759cd240
Kyuling Lee <kyuling@me.com>
2018-06-02 15:50:37 +0900

    인벤토리 아이템 클래스 생성

---
+++ Engine/InventoryItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    //인벤토리 아이템
+    public class InventoryItem
+    {
+        //인벤토리 요소
+        public Item Details { get; set; }
+        public int Quantity { get; set; }
+
+        //생성자
+        public InventoryItem(Item details, int quantity)
+        {
+            Details = details;
+            Quantity = quantity;
+        }
+
+    }
+}
