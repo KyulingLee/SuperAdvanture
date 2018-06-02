6d0edf38da29d9b89dc64a35fffc41f68b01fa62
Kyuling Lee <kyuling@me.com>
2018-06-02 15:52:49 +0900

    전리품 아이템 클래스 생성

---
+++ Engine/LootItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    //전리품
+    public class LootItem
+    {
+        public Item Details { get; set; }
+        public int DropPercentage { get; set; }
+        public bool IsDefaultItem { get; set; }
+
+        //생성자
+        public LootItem(Item details, int dropPercentage, bool isDefaultItem)
+        {
+            Details = details;
+            DropPercentage = dropPercentage;
+            IsDefaultItem = isDefaultItem;
+        }
+    }
+}
