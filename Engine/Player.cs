9bba2351093706b8a4e7b424c9de7f8ba9c1831f
Kyuling Lee <kyuling@me.com>
2018-06-02 15:57:23 +0900

    사용자에게 인벤토리와 퀘스트 목록을 받아올 수 있도록 한다. 구조를 그냥 리스트를 이용하였음.

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -18,12 +18,22 @@ namespace Engine
         //Level
         public int Level { get; set; }
 
+        //사용자 인벤토리
+        public List<InventoryItem> Inventory { get; set; }
+
+        //사용자 퀘스트
+        public List<PlayerQuest> Quests { get; set; }
+
         //자식 생성자
         public Player(int currentHitPoints, int maximumHitPoints, int gold, int experiencePoints, int level) : base (currentHitPoints, maximumHitPoints)
         {
             Gold = gold;
             ExperiencePoints = experiencePoints;
             Level = level;
+
+            //인벤토리, 퀘스트 생성
+            Inventory = new List<InventoryItem>();
+            Quests = new List<PlayerQuest>();
         }
     }
 }
