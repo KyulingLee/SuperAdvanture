6382db6ad57a4aa41c48c018f2d6589b4c3a478d
Kyuling Lee <kyuling@me.com>
2018-06-02 15:54:28 +0900

    사용자 퀘스트 클래스 추가

---
+++ Engine/PlayerQuest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    //사용자 퀘스트
+    public class PlayerQuest
+    {
+        public Quest Details { get; set; }
+        public bool IsCompleted { get; set; }
+
+        //생성자
+        public PlayerQuest(Quest details)
+        {
+            Details = details;
+            IsCompleted = false;
+        }
+    }
+}
