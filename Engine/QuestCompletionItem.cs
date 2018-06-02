0bfdbe67294cef950da4b79754a64b8ff6dca873
Kyuling Lee <kyuling@me.com>
2018-06-02 15:55:30 +0900

    퀘스트 완료 관련된 아이템들을 관리하는 클래스 생성.

---
+++ Engine/QuestCompletionItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    //퀘스트 완료 아이템
+    public class QuestCompletionItem
+    {
+        public Item Details { get; set; }
+        public int Quantity { get; set; }
+
+        //생성자
+        public QuestCompletionItem(Item details, int quantity)
+        {
+            Details = details;
+            Quantity = quantity;
+        }
+    }
+}
