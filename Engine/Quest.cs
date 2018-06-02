a51b9f0122e0ee1193f779ef7bd60a671ab1c826
Kyuling Lee <kyuling@me.com>
2018-06-02 15:30:02 +0900

    퀘스트 보상에 아이템을 받을 수 있도록 프로퍼티 추가

--- Engine/Quest.cs
+++ Engine/Quest.cs
@@ -6,13 +6,18 @@ using System.Threading.Tasks;
 
 namespace Engine
 {
+    //퀘스트
     public class Quest
     {
+        //퀘스트 기본 정보
         public int ID { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public int RewardExperiencePoints { get; set; }
         public int RewardGold { get; set; }
+
+        //아이템 보상에 대해서 추가
+        public Item RewardItem { get; set; }
 
         //생성자
         public Quest(int id, string name, string descriptopn, int rewardExperiencePoints, int rewardGold)
