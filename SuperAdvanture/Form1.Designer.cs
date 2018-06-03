842ac44cb0dea578853249d1e7d4a538f9ebdeda
Kyuling Lee <kyuling@me.com>
2018-06-03 15:19:10 +0900

    게임 화면 디자인 구성 완료.

--- SuperAdvanture/Form1.Designer.cs
+++ SuperAdvanture/Form1.Designer.cs
@@ -36,6 +36,21 @@
             this.ExperienceLabel = new System.Windows.Forms.Label();
             this.GoldLabel = new System.Windows.Forms.Label();
             this.HitPointsLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.WeaponComboBox = new System.Windows.Forms.ComboBox();
+            this.PotionComboBox = new System.Windows.Forms.ComboBox();
+            this.UseWeaponButton = new System.Windows.Forms.Button();
+            this.UsePotionButton = new System.Windows.Forms.Button();
+            this.NorthButton = new System.Windows.Forms.Button();
+            this.EastButton = new System.Windows.Forms.Button();
+            this.SouthButton = new System.Windows.Forms.Button();
+            this.WestButton = new System.Windows.Forms.Button();
+            this.LocationRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.MessageRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.InventoryDataGridView = new System.Windows.Forms.DataGridView();
+            this.QuestsDataGridView = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.InventoryDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QuestsDataGridView)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -110,11 +125,155 @@
             this.HitPointsLabel.TabIndex = 4;
             this.HitPointsLabel.Text = "0";
             // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(617, 531);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Select Action";
+            // 
+            // WeaponComboBox
+            // 
+            this.WeaponComboBox.FormattingEnabled = true;
+            this.WeaponComboBox.Location = new System.Drawing.Point(369, 559);
+            this.WeaponComboBox.Name = "WeaponComboBox";
+            this.WeaponComboBox.Size = new System.Drawing.Size(245, 23);
+            this.WeaponComboBox.TabIndex = 9;
+            // 
+            // PotionComboBox
+            // 
+            this.PotionComboBox.FormattingEnabled = true;
+            this.PotionComboBox.Location = new System.Drawing.Point(369, 593);
+            this.PotionComboBox.Name = "PotionComboBox";
+            this.PotionComboBox.Size = new System.Drawing.Size(245, 23);
+            this.PotionComboBox.TabIndex = 10;
+            // 
+            // UseWeaponButton
+            // 
+            this.UseWeaponButton.Location = new System.Drawing.Point(620, 559);
+            this.UseWeaponButton.Name = "UseWeaponButton";
+            this.UseWeaponButton.Size = new System.Drawing.Size(75, 23);
+            this.UseWeaponButton.TabIndex = 11;
+            this.UseWeaponButton.Text = "Use";
+            this.UseWeaponButton.UseVisualStyleBackColor = true;
+            // 
+            // UsePotionButton
+            // 
+            this.UsePotionButton.Location = new System.Drawing.Point(620, 593);
+            this.UsePotionButton.Name = "UsePotionButton";
+            this.UsePotionButton.Size = new System.Drawing.Size(75, 23);
+            this.UsePotionButton.TabIndex = 12;
+            this.UsePotionButton.Text = "Use";
+            this.UsePotionButton.UseVisualStyleBackColor = true;
+            // 
+            // NorthButton
+            // 
+            this.NorthButton.Location = new System.Drawing.Point(493, 433);
+            this.NorthButton.Name = "NorthButton";
+            this.NorthButton.Size = new System.Drawing.Size(75, 23);
+            this.NorthButton.TabIndex = 13;
+            this.NorthButton.Text = "North";
+            this.NorthButton.UseVisualStyleBackColor = true;
+            // 
+            // EastButton
+            // 
+            this.EastButton.Location = new System.Drawing.Point(573, 457);
+            this.EastButton.Name = "EastButton";
+            this.EastButton.Size = new System.Drawing.Size(75, 23);
+            this.EastButton.TabIndex = 14;
+            this.EastButton.Text = "East";
+            this.EastButton.UseVisualStyleBackColor = true;
+            // 
+            // SouthButton
+            // 
+            this.SouthButton.Location = new System.Drawing.Point(493, 487);
+            this.SouthButton.Name = "SouthButton";
+            this.SouthButton.Size = new System.Drawing.Size(75, 23);
+            this.SouthButton.TabIndex = 15;
+            this.SouthButton.Text = "South";
+            this.SouthButton.UseVisualStyleBackColor = true;
+            // 
+            // WestButton
+            // 
+            this.WestButton.Location = new System.Drawing.Point(412, 457);
+            this.WestButton.Name = "WestButton";
+            this.WestButton.Size = new System.Drawing.Size(75, 23);
+            this.WestButton.TabIndex = 16;
+            this.WestButton.Text = "West";
+            this.WestButton.UseVisualStyleBackColor = true;
+            // 
+            // LocationRichTextBox
+            // 
+            this.LocationRichTextBox.Location = new System.Drawing.Point(347, 19);
+            this.LocationRichTextBox.Name = "LocationRichTextBox";
+            this.LocationRichTextBox.ReadOnly = true;
+            this.LocationRichTextBox.Size = new System.Drawing.Size(360, 105);
+            this.LocationRichTextBox.TabIndex = 17;
+            this.LocationRichTextBox.Text = "";
+            // 
+            // MessageRichTextBox
+            // 
+            this.MessageRichTextBox.Location = new System.Drawing.Point(347, 130);
+            this.MessageRichTextBox.Name = "MessageRichTextBox";
+            this.MessageRichTextBox.ReadOnly = true;
+            this.MessageRichTextBox.Size = new System.Drawing.Size(360, 286);
+            this.MessageRichTextBox.TabIndex = 18;
+            this.MessageRichTextBox.Text = "";
+            // 
+            // InventoryDataGridView
+            // 
+            this.InventoryDataGridView.AllowUserToAddRows = false;
+            this.InventoryDataGridView.AllowUserToDeleteRows = false;
+            this.InventoryDataGridView.AllowUserToResizeRows = false;
+            this.InventoryDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.InventoryDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.InventoryDataGridView.Enabled = false;
+            this.InventoryDataGridView.Location = new System.Drawing.Point(16, 130);
+            this.InventoryDataGridView.MultiSelect = false;
+            this.InventoryDataGridView.Name = "InventoryDataGridView";
+            this.InventoryDataGridView.ReadOnly = true;
+            this.InventoryDataGridView.RowHeadersVisible = false;
+            this.InventoryDataGridView.RowTemplate.Height = 27;
+            this.InventoryDataGridView.Size = new System.Drawing.Size(312, 309);
+            this.InventoryDataGridView.TabIndex = 19;
+            // 
+            // QuestsDataGridView
+            // 
+            this.QuestsDataGridView.AllowUserToAddRows = false;
+            this.QuestsDataGridView.AllowUserToDeleteRows = false;
+            this.QuestsDataGridView.AllowUserToResizeRows = false;
+            this.QuestsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.QuestsDataGridView.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.QuestsDataGridView.Enabled = false;
+            this.QuestsDataGridView.Location = new System.Drawing.Point(16, 446);
+            this.QuestsDataGridView.MultiSelect = false;
+            this.QuestsDataGridView.Name = "QuestsDataGridView";
+            this.QuestsDataGridView.ReadOnly = true;
+            this.QuestsDataGridView.RowHeadersVisible = false;
+            this.QuestsDataGridView.RowTemplate.Height = 27;
+            this.QuestsDataGridView.Size = new System.Drawing.Size(312, 189);
+            this.QuestsDataGridView.TabIndex = 20;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(717, 643);
+            this.Controls.Add(this.QuestsDataGridView);
+            this.Controls.Add(this.InventoryDataGridView);
+            this.Controls.Add(this.MessageRichTextBox);
+            this.Controls.Add(this.LocationRichTextBox);
+            this.Controls.Add(this.WestButton);
+            this.Controls.Add(this.SouthButton);
+            this.Controls.Add(this.EastButton);
+            this.Controls.Add(this.NorthButton);
+            this.Controls.Add(this.UsePotionButton);
+            this.Controls.Add(this.UseWeaponButton);
+            this.Controls.Add(this.PotionComboBox);
+            this.Controls.Add(this.WeaponComboBox);
+            this.Controls.Add(this.label5);
             this.Controls.Add(this.LevelLabel);
             this.Controls.Add(this.ExperienceLabel);
             this.Controls.Add(this.GoldLabel);
@@ -124,7 +283,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Kyuling\'s Game";
+            this.Text = "Kyuling\'s RPG Game";
+            ((System.ComponentModel.ISupportInitialize)(this.InventoryDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.QuestsDataGridView)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -140,6 +301,19 @@
         private System.Windows.Forms.Label ExperienceLabel;
         private System.Windows.Forms.Label GoldLabel;
         private System.Windows.Forms.Label HitPointsLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox WeaponComboBox;
+        private System.Windows.Forms.ComboBox PotionComboBox;
+        private System.Windows.Forms.Button UseWeaponButton;
+        private System.Windows.Forms.Button UsePotionButton;
+        private System.Windows.Forms.Button NorthButton;
+        private System.Windows.Forms.Button EastButton;
+        private System.Windows.Forms.Button SouthButton;
+        private System.Windows.Forms.Button WestButton;
+        private System.Windows.Forms.RichTextBox LocationRichTextBox;
+        private System.Windows.Forms.RichTextBox MessageRichTextBox;
+        private System.Windows.Forms.DataGridView InventoryDataGridView;
+        private System.Windows.Forms.DataGridView QuestsDataGridView;
     }
 }
 
